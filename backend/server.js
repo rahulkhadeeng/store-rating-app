72ce66a60d86e12d843dfccced5b5bc928d3940c
Rahul Khade <rahulkhadeeng@gmail.com>
2026-06-01 14:04:01 +0530

    setup: backend and frontend project structure and env

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
