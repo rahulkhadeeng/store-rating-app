05592a4fc37148c2aca715cc0d906f522c926157
Rahul Khade <rahulkhadeeng@gmail.com>
2026-06-02 22:51:56 +0530

    feat: implemented user registeration frontend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,97 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+
 function Signup() {
+
+    const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    address: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post(
+        "/auth/register",
+        formData
+      );
+
+      alert(res.data.message);
+
+      navigate("/");
+    } catch (error) {
+      alert(
+        error.response?.data?.message ||
+        "Registration failed"
+      );
+    }
+  };
+
   return (
     <div>
       <h1>Signup Page</h1>
+    
+    <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="text"
+          name="address"
+          placeholder="Address"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Signup
+        </button>
+      </form>
+
+      <br />
+
+      <Link to="/">
+        Already have an account? Login
+      </Link>
+
     </div>
   );
 }
