9335450a0a5b032cb8edf9e87b4a785a4d972f81
Rahul Khade <rahulkhadeeng@gmail.com>
2026-06-02 22:36:34 +0530

    feat: Implement frontend login and role based all navigations

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,81 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/auth/login", formData);
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.role);
+
+      if (res.data.role === "ADMIN") {
+        navigate("/admin");
+      } else if (res.data.role === "STORE_OWNER") {
+        navigate("/owner");
+      } else {
+        navigate("/user");
+      }
+    } catch (error) {
+      alert(
+        error.response?.data?.message ||
+        "Login failed"
+      );
+    }
+  };
+
+
   return (
     <div>
       <h1>Login Page</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+
+      <br />
+
+      <Link to="/signup">
+        Create Account
+      </Link>
+
     </div>
   );
 }
