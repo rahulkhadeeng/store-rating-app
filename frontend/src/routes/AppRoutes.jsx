e728c3a6b174dc5c23270ec05e4a85024b8c1ef1
Rahul Khade <rahulkhadeeng@gmail.com>
2026-06-02 23:21:51 +0530

    feat: Add frontend route protection and role based access

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,14 +6,37 @@ import AdminDashboard from "../pages/AdminDashboard";
 import UserDashboard from "../pages/UserDashboard";
 import StoreOwnerDashboard from "../pages/StoreOwnerDashboard";
 
+import ProtectedRoute from "../components/ProtectedRoute";
+    
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/user" element={<UserDashboard />} />
-      <Route path="/owner" element={<StoreOwnerDashboard />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute allowedRole="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/user"
+        element={
+          <ProtectedRoute allowedRole="user">
+            <UserDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/owner"
+        element={
+          <ProtectedRoute allowedRole="owner">
+            <StoreOwnerDashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
